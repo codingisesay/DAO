fbdc3ecf46298b273192bb0c6b2a6982264eb335
Akash Singh <akashsngh681681@gmail.com>
2025-05-09 15:15:04 +0530

    implement login backend

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,11 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('name',50);
+            $table->string('email',50)->unique();
+            // $table->timestamp('email_verified_at')->nullable();
+            $table->string('password',50);
+            $table->unsignedBigInteger('user_type_id'); 
+            $table->foreign('user_type_id')->references('id')->on('user_types')->onDelete('cascade');
+            // $table->rememberToken();
             $table->timestamps();
         });
 
