b80211969ebcb27038a10aa921c25d7ece1fca98
Akash Singh <akashsngh681681@gmail.com>
2025-05-13 17:23:39 +0530

    implementing discovery server in laravel app

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema; 
+use Illuminate\Support\Facades\Http;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +23,12 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Schema::defaultStringLength(191);
+        $discoveryServerUrl = env('DISCOVERY_SERVER_URL', 'http://172.16.1.224:8061/eureka/'); // Replace with actual Discovery Server URL
+
+        Http::post("{$discoveryServerUrl}/register", [
+            'name' => 'dao-laravel-backend',
+            'url' => config('app.url'),
+            'healthcheck' => config('app.url') . '/api/health',
+        ]);
     }
 }
