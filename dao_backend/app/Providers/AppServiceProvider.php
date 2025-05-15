645d2ae6c80284d482c6c8f53b9a07fa5e9f36fd
Akash Singh <akashsngh681681@gmail.com>
2025-05-15 17:34:09 +0530

    creating migration application table

--- dao_backend/app/Providers/AppServiceProvider.php
+++ dao_backend/app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use App\Services\EurekaService;
+use Illuminate\Support\Facades\Schema; 
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +22,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
         app(EurekaService::class)->register();
     
         // Optionally schedule heartbeats using Laravel scheduler
