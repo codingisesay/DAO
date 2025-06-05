4a652f79426c8ee59f9b4e7bf6f88336fb57041b
Akash Singh <akashsngh681681@gmail.com>
2025-06-05 12:09:27 +0530

    all pending table data

--- dao_backend/app/Http/Controllers/AdminController.php
+++ dao_backend/app/Http/Controllers/AdminController.php
@@ -29,4 +29,26 @@ class AdminController extends Controller
     ],200);
 }
 
+public function getPendingApplications()
+{
+    $pendingApplications = DB::table('customer_appliction_status')
+        ->join('customer_application_details', 'customer_appliction_status.application_id', '=', 'customer_application_details.id')
+        ->select(
+            'customer_appliction_status.*',
+            'customer_application_details.first_name as first_name',
+            'customer_application_details.middle_name as middle_name',
+            'customer_application_details.last_name as last_name',
+            'customer_application_details.created_at as created_at',
+            'customer_application_details.application_no as application_no',
+            //need to join user table to get the agent name
+            
+        )
+        ->where('customer_appliction_status.status', 'pending')
+        ->get();
+
+    return response()->json([
+        'data' => $pendingApplications
+    ], 200);
+
+}
 }
