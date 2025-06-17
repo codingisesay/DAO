109aeb72571706ebaac265bc8ef830c9658edafb
Ankita <pansare.ankita04@gmail.com>
2025-06-17 15:54:47 +0530

    logo changed for agent dashboard

--- front-end/src/pages/Agentpages/AgentDashboard.jsx
+++ front-end/src/pages/Agentpages/AgentDashboard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useAuth } from '../../auth/AuthContext';
 import { useNavigate } from 'react-router-dom';
-import payvanceLogo from '../../assets/imgs/payvance_light_logo.png';
+import payvanceLogo from '../../assets/imgs/payvance_dark_logo.png';
 import ThemeToggle from '../../components/Toggle';
 import useLocalStorage from "use-local-storage";
 import AccountBarChart from './AccountBarChart';
