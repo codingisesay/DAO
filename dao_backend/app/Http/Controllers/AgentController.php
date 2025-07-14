aa04e70bb1f43ffa8a47206d0ba04ae579452dd0
mahaddalkar-om <om.m@siltech.co.in>
2025-07-14 10:42:57 +0530

    updating api

--- dao_backend/app/Http/Controllers/AgentController.php
+++ dao_backend/app/Http/Controllers/AgentController.php
@@ -799,6 +799,55 @@ public function getPendingApplicationsByAgent($agentId)
     ]);
 }
 
+
+// data for the table based on status and agent
+// eg --
+// agent/applications/by-status/123/review
+// agent/applications/by-status/123/rejected
+// agent/applications/by-status/123/approved 
+public function getApplicationsByAgentAndStatusForTable($agentId, $status)
+{
+    $applications = DB::table('customer_appliction_status')
+        ->leftJoin('customer_application_details', 'customer_appliction_status.application_id', '=', 'customer_application_details.id')
+        ->leftJoin('account_personal_details', 'customer_appliction_status.application_id', '=', 'account_personal_details.application_id')
+        ->leftJoin('application_address_details', 'customer_appliction_status.application_id', '=', 'application_address_details.application_id')
+        ->leftJoin('agent_live_photos', 'customer_appliction_status.application_id', '=', 'agent_live_photos.application_id')
+        ->leftJoin('applicant_live_photos', 'customer_appliction_status.application_id', '=', 'applicant_live_photos.application_id')
+        ->leftJoin('application_personal_details', 'customer_appliction_status.application_id', '=', 'application_personal_details.application_id')
+        ->leftJoin('application_service_status', 'customer_appliction_status.application_id', '=', 'application_service_status.application_id')
+        ->leftJoin('document_approved_status', 'customer_appliction_status.application_id', '=', 'document_approved_status.application_id')
+        ->leftJoin('nominee_approved_status', 'customer_appliction_status.application_id', '=', 'nominee_approved_status.application_id')
+        ->where('customer_application_details.agent_id', $agentId)
+        ->where('customer_appliction_status.status', $status)
+        ->select(
+            'customer_application_details.*',
+            'customer_application_details.id as application_id',
+            'customer_appliction_status.status as full_application_status',
+            'account_personal_details.status as account_personal_details_status',
+            'account_personal_details.status_comment as account_personal_details_status_comment',
+            'application_address_details.status as application_address_details_status',
+            'application_address_details.status_comment as application_address_details_status_comment',
+            'agent_live_photos.status as agent_live_photos_status',
+            'agent_live_photos.status_comment as agent_live_photos_status_comment',
+            'applicant_live_photos.status as applicant_live_photos_status',
+            'applicant_live_photos.status_comment as applicant_live_photos_status_comment',
+            'application_personal_details.status as application_personal_details_status',
+            'application_personal_details.status_comment as application_personal_details_status_comment',
+            'application_service_status.status as application_service_status_status',
+            'application_service_status.status_comment as application_service_status_status_comment',
+            'document_approved_status.status as document_approved_status_status',
+            'document_approved_status.status_comment as document_approved_status_status_comment',
+            'nominee_approved_status.status as nominee_approved_status_status',
+            'nominee_approved_status.status_comment as nominee_approved_status_status_comment'
+        )
+        ->get();
+
+    return response()->json([
+        'message' => ucfirst($status) . ' applications fetched successfully.',
+        'data' => $applications
+    ]);
+}
+
 public function getReviewApplicationsByAgent($agentId)
 {
     $applications = DB::table('customer_appliction_status')
--- dao_backend/routes/api.php
+++ dao_backend/routes/api.php
@@ -153,6 +153,8 @@ Route::post('admin/kyc-application-status/update', [kycAgentController::class, '
 //This is for getting the application status by agent id
 Route::get('/agent/dashboardApplicationStatus/{agent_id}', [AgentController::class, 'getApplicationStatusByAgents']);
 // dashboard agent routes 
+// This is for getting the data for the table based on status and agent
+Route::get('agent/cardstable/by-status/{agentId}/{status}', [AgentController::class, 'getApplicationsByAgentAndStatusForTable']);
 // status count 
 Route::get('/account-status-by-agent', [AgentController::class, 'getAccountStatusByAgent']);
 // all applications for the agent
